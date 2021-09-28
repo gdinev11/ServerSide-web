f35b16500700fa5080af771b4946702973a0b7c4
gdinev11 <89752726+gdinev11@users.noreply.github.com>
2021-09-28 18:48:10 -0500

    Initial commit

---
+++ src/main/java/edu/neiu/finalprojsswd/FinalprojsswdApplication.java
@@ -0,0 +1,13 @@
+package edu.neiu.finalprojsswd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojsswdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojsswdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/neiu/finalprojsswd/FinalprojsswdApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neiu.finalprojsswd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojsswdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
