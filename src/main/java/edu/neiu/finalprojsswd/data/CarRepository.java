cd78076107cbfba39ee9ceb77f9b5baf7f0d4bd3
gdinev11 <89752726+gdinev11@users.noreply.github.com>
2021-10-28 15:40:04 -0500

    Part I. ORM

---
+++ src/main/java/edu/neiu/finalprojsswd/data/CarRepository.java
@@ -0,0 +1,8 @@
+package edu.neiu.finalprojsswd.data;
+
+import edu.neiu.finalprojsswd.models.Car;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository<Car, long> {
+
+}
--- src/main/java/edu/neiu/finalprojsswd/models/Car.java
+++ src/main/java/edu/neiu/finalprojsswd/models/Car.java
@@ -1,10 +1,20 @@
 package edu.neiu.finalprojsswd.models;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Car {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
     private String make;
     private String mode;
     private int year;
+    private LocalDateTime created;
+    private LocalDateTime modified;
+
 
     public Car() {
         this.make = "";
@@ -41,7 +51,36 @@ public class Car {
     public void setYear(int year) {
         this.year = year;
     }
-    @Override
+
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
+
+    @PrePersist
+    public void onCreate {
+        this.setCreated(LocalDateTime.now());
+        this.setModified(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    public void onUpdate{
+        this.setModified(LocalDateTime.now());
+    }
+
+
     public String toString(){
         return "Your Car:";
     }
